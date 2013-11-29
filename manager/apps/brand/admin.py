cefa60d14bc85b35af061b7a082f1e818a3a11aa
Feng-Gao <gaofeng860918@gmail.com>
2013-11-29 15:38:44 +0800

    [admin,okfn/brand-manager#36] very first experiment to add the delete button and show a popup dialog for confirmation
    the confirmation in fact did *nothing* at the moment since we do not know exactly how to *delete* brand or owner

--- manager/apps/brand/admin.py
+++ manager/apps/brand/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 from django import forms
 from .models import Brand, BrandOwner, BrandType
 
+#customized delete button to be placed on each row of the ModelAdmin
+def delete_button(obj):
+    #TODO: need a better name other than 'soft delete' or 'delete'
+    return "<input type='button' value='Soft Delete' id='customized_delete_button' onClick='softDeleteConfirmation()'/>"
+
+#TODO: need a better name other than 'soft delete' or 'delete'
+delete_button.short_description = 'Soft Delete'
+delete_button.allow_tags = True
 
 class BrandTypeAdmin(admin.ModelAdmin):
     actions = None
@@ -15,10 +23,15 @@ admin.site.register(BrandType, BrandTypeAdmin)
 
 class BrandOwnerAdmin(admin.ModelAdmin):
     actions = None
-    list_display = ('owner_nm', 'owner_logo_admin', 'owner_link')
+    list_display = ('owner_nm', 'owner_logo_admin', 'owner_link',delete_button)
     fields = (
         'owner_nm', 'owner_link', 'owner_wiki_en', 'owner_logo')
     search_fields = ['owner_nm']
+
+    class Media:
+        js = (
+            'brand/js/customized_admin.js',   
+        )
 
     # Never delete a brand, update its BSIN
     def has_delete_permission(self, request, obj=None):
@@ -29,13 +42,18 @@ admin.site.register(BrandOwner, BrandOwnerAdmin)
 
 class BrandAdmin(admin.ModelAdmin):
     actions = None
-    list_display = ('bsin', 'brand_nm', 'brand_logo_admin', 'flag_delete')
+    list_display = ('bsin', 'brand_nm', 'brand_logo_admin', 'flag_delete',delete_button)
     fields = (
         'bsin', 'brand_nm', 'owner_cd', 'brand_type_cd', 'brand_link',
         'brand_logo', ('flag_delete', 'comments'), 'last_modified')
     readonly_fields = ('bsin', 'last_modified')
     search_fields = ['bsin', 'brand_nm', 'owner_cd__owner_nm']
     list_filter = ('flag_delete', )
+
+    class Media:
+        js = (
+            'brand/js/customized_admin.js',   
+        )
 
     # Never delete a brand, update its BSIN
     def has_delete_permission(self, request, obj=None):
---
+++ manager/apps/brand/static/brand/js/customized_admin.js
@@ -0,0 +1,4 @@
+function softDeleteConfirmation() {
+    if(confirm('Are you sure you want to soft delete it?'))
+        alert('Has been deleted!');
+}
